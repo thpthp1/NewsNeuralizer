9ad8108e6d9cc8a6be6d7c402bfb06c75ecae4ea
Phan Trinh <phan181299@gmail.com>
2021-03-05 23:19:21 -0500

    New news feed api dummy sample and fixed predict api

--- news_src/api/urls.py
+++ news_src/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict', views.predict)
+    path('predict', views.predict),
+    path('news-feed', views.feed)
 ]
--- news_src/api/views.py
+++ news_src/api/views.py
@@ -1,14 +1,55 @@
+from json.decoder import JSONDecodeError
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import random
 import json
+import requests
+import os
+import datetime
+import time
+import logging
 
+PAGES_LOOKUP = 3
+REQUEST_WAIT_TIME = 0.5
+loggger = logging.getLogger('app_api')
 
 def predict(request):
-    body_unicode = request.body.decode('utf-8')
-    body = json.loads(body_unicode)
-    if 'title' not in body or 'selftext' not in body:
-        return JsonResponse({'error': 'Missing Parameters'})
+    try:
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        if 'title' not in body or 'selftext' not in body:
+            return JsonResponse({'error': 'Missing Parameters'})
+    except JSONDecodeError as e:
+        return JsonResponse({'error': 'Parse body failure', 'msg': e.msg})
     proba = random.random()
     result = {'prediction': proba > 0.5, 'proba': proba}
+    loggger.debug(result)
     return JsonResponse(result)
+
+def feed(request):
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    return_feed = []
+    for page in range(1, PAGES_LOOKUP+1):
+        r = requests.get(f"https://newsapi.org/v2/everything?sources=cnn,fox-news,axios\
+                            &from={yesterday.isoformat()}&to={today.isoformat}&\
+                            language=en&page={page}&apiKey={os.environ['NEWS_API_KEY']}")
+        news_feed = r.json()
+        if not r.ok or news_feed["status"] != 'ok':
+            return JsonResponse({'error': 'External Service Error'})
+        __build_return_feed(news_feed, return_feed)
+        time.sleep(REQUEST_WAIT_TIME)
+    return JsonResponse({"count": len(return_feed), "feed": return_feed})
+
+def __build_return_feed(news_feed, return_feed):
+    for articles in news_feed['articles']:
+        #dummy prediction
+        proba = random.random()
+        return_feed.append(
+            {
+                "text": articles["content"] if articles["content"] is not None else articles["title"],
+                "url": articles["url"],
+                'prediction': proba > 0.5, 
+                'proba': proba
+            }
+        )
