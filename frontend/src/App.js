2f28fc28a3983b7e191cb026bae0d39ee693e2a1
BertramSu <40878105+BertramSu@users.noreply.github.com>
2021-04-07 14:08:21 -0500

    Changed_Noahs_form
    Changed Noah's form to the one I had been using for posting.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Welcome from './components/sections/Welcome';
 import ArticleForm from './components/sections/ArticleForm';
+import ManualForm from './index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
        <>
         <Welcome />
-        <ArticleForm />
+        <ManualForm />
        </>
   );
 }
