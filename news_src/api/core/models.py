f5842e7e9a1b9abafa0317b0f2d04b0bfe19bc65
Phan Trinh <phan181299@gmail.com>
2021-04-18 16:33:34 -0400

    Fix on prediction (text + title) and datepublish null problem

--- news_src/api/core/models.py
+++ news_src/api/core/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.db.models.fields import BooleanField, CharField, FloatField, IntegerField, TextField, TimeField, URLField
+from django.db.models.fields import BooleanField, CharField, DateTimeField, FloatField, IntegerField, TextField, TimeField, URLField
 
 class News(models.Model):
     id = IntegerField(primary_key=True)
     text = TextField()
+    title = TextField(default="")
     url = URLField(unique=True)
-    image = URLField()
-    date_publish = TimeField()
+    image = URLField(null=True)
+    date_publish = DateTimeField(null=True)
     source_domain = CharField(max_length=50)
