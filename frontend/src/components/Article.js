1cf425ada1f323ac0654df8568f07ca08e1fe71b
Noah Friedman <noahf3744@gmail.com>
2021-05-10 12:11:10 -0400

    Adjusted article cards to have smaller images
    + Changed title to News Neuralizer

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './Article.css'
 
 //One article for the news feed
 function Article(props){
     return(
       <div className="article card h-100 shadow bg-white rounded">
         <div className="card-body d-flex flex-column">
-          <img class="card-img-top" src={props.image} alt="Card image cap" ></img>
-          <h2 className="card-title">{props.title}</h2>
+          <div className="head-row-container">
+            
+            <div className="head-row">
+              <img class="card-img-top" src={props.image} alt="Card image cap" ></img>
+            </div>
+            <div className="head-row">
+              <h2 className="card-title">{props.title}</h2>
+            </div>
+          </div>
           <p className="probability">{props.prediction} {isNaN(props.probability) ? props.probability : parseFloat(props.probability * 100).toFixed(0) + '%'}</p>
           <p className="card-text">{props.body}</p>
           <a href={props.url} target="_blank" rel="noreferrer noopener" className="btn btn-md btn-outline-primary mt-auto">Visit Source</a>
